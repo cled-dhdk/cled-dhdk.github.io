0cb87112b35f7397b8e4d2fe80e03ae978230d94
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2019-05-10 15:50:50 +0200

    Update scroll-bar.js

--- js/scroll-bar.js
+++ js/scroll-bar.js
@@ -1,15 +1,22 @@
 window.onscroll = function() {scrollFunction()};
-var menuList = document.getElementById("example-menu");
+var menuList = document.getElementById("topbar-center-logo");
+var logoOfDesk = document.getElementById("logo-to-delete");
 var toggleBar = document.getElementById("toggle-bar");
-var header = document.getElementById("list");
-var selectedItem = header.getElementsByClassName("item");
+var logo = document.getElementById("logo");
+var hamburger = document.getElementById("hamburger");
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     menuList.style.backgroundColor = "#f8f9fb";
+    logoOfDesk.style.display = "none";
     toggleBar.style.backgroundColor = "#f8f9fb";
+    logo.style.display = "none";
+    hamburger.style.marginTop = "2%";
   } else {
     menuList.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    logoOfDesk.style.display = "initial";
     toggleBar.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    logo.style.display = "initial";
+    hamburger.style.marginTop = "-8%";
   }
 }
